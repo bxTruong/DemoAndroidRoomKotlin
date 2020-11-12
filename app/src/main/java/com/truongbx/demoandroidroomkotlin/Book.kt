06b1b7ac0865e3926b6e6cf006dfd29cefa38ab9
Bùi Xuân Trường <truongmagic9a@gmail.com>
2020-11-12 07:58:13 +0700

    1.0.2 Readfile database availabe

--- app/src/main/java/com/truongbx/demoandroidroomkotlin/Book.kt
+++ app/src/main/java/com/truongbx/demoandroidroomkotlin/Book.kt
@@ -3,6 +3,7 @@ package com.truongbx.demoandroidroomkotlin
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 import java.io.Serializable
 
 @Entity(tableName = "bookData")
--- app/src/main/java/com/truongbx/demoandroidroomkotlin/BookDatabase.kt
+++ app/src/main/java/com/truongbx/demoandroidroomkotlin/BookDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Book::class], version = 1, exportSchema = false)
+@Database(entities = [Book::class], version = 1, exportSchema = true)
 abstract class BookDatabase : RoomDatabase() {
+    // để đọc một file có sẵn data: sửa exportSchema=true, còn lưu bình thường để false, các trường dữ liệu trong file phải có notnull vì android room mặc định đã có notnull
     abstract fun userDAO(): BookDAO
 
     companion object {
@@ -18,8 +19,9 @@ abstract class BookDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     BookDatabase::class.java,
-                    "book_database"
-                ).build()
+                    "book.db"
+                ).createFromAsset("book_available.db")
+                    .build()
                 INSTANCE = instance
                 instance
             }
